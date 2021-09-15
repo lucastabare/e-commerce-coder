ff6bc08d38a5d3d96dffd42064b872c76db081c2
Lucas Tabare <lucas.tabare@gmail.com>
2021-09-15 20:39:18 -0300

    desafio 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>HOLA CULIA</h1>
+      <h1>E-commerce Fenix</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid quia tempore, quidem mollitia sapiente obcaecati 
         dolores velit distinctio sed dolorem officiis asperiores laborum perferendis harum suscipit omnis dolorum earum sequi.</p>
     </div>
