86b906a7e95ec249e84db5095dcd34416e3e2ec0
Lucas Tabare <lucas.tabare@gmail.com>
2021-09-20 22:04:58 -0300

    agregando el NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from "./logo.svg";
+import React, { Component, Fragment } from "react";
 import "./App.css";
-
+import NavBar from "./componets/navBar.js";
+import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1>E-commerce Fenix</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid quia tempore, quidem mollitia sapiente obcaecati 
         dolores velit distinctio sed dolorem officiis asperiores laborum perferendis harum suscipit omnis dolorum earum sequi.</p>
---
+++ src/componets/navBar.js
@@ -0,0 +1,64 @@
+import React, { Component, Fragment } from "react";
+import logo from "../logo.png";
+
+const NavBar = () => {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div className="container-fluid ">
+          <img src={logo} alt="" className="logo" />
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="navbar-brand" aria-current="page" href="#">
+                  Marcas
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="navbar-brand" href="#">
+                  Contacto
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="navbar-brand" aria-current="page" href="#">
+                  Mis Pedidos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="navbar-brand" aria-current="page" href="#">
+                  ARMA TU PC
+                </a>
+              </li>
+            </ul>
+
+            <form className="d-flex" name="Form">
+              <input
+                className="form-control me-2"
+                type="text"
+                placeholder="Busca Un Prodcuto"
+                name=" busqueda"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Buscar
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
